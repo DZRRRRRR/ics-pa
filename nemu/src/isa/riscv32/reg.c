fb2ce662be85ebdaa855b2d858c1930f3252ac5a
tracer-ics2024 <tracer@njuics.org>
2024-12-28 01:51:16 +0800

    >  compile NEMU 231220000 张三 Linux chus-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:51:16 up  6:01,  1 user,  load average: 0.03, 0.04, 0.08

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -15,7 +15,7 @@
 
 #include <isa.h>
 #include "local-include/reg.h"
-
+#include "macro.h"
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
@@ -24,7 +24,8 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
- printf("regs size %d\r\n",11);
+ int len = ARRLEN(regs);
+ printf("regs size %d\r\n",len);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
